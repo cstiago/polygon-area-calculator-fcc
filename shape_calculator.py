17929f61b8556267ccb97dd1f08b94092326c1f6
Tiago Cunha <121774112+cstiago@users.noreply.github.com>
2023-03-01 14:45:52 -0300

    feat(calculator): add rectangle blank methods

--- shape_calculator.py
+++ shape_calculator.py
@@ -1,3 +1,30 @@
 class Rectangle:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+    
+    def set_width(self):
+        return 0
+    
+    def set_height(self):
+        return 0
+    
+    def get_area(self):
+        return 0
+    
+    def get_perimeter(self):
+        return 0
+    
+    def get_diagonal(self):
+        return 0
+    
+    def get_picture(self):
+        return 0
+    
+    def get_amount_inside(self):
+        return 0
+    
+    def __str__(self):
+        return ''
 
-class Square:
+class Square(Rectangle):

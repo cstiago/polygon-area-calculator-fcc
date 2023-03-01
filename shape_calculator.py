799297c8adf77e8a4d3e2f98930beee4c376e45b
Tiago Cunha <121774112+cstiago@users.noreply.github.com>
2023-03-01 15:27:01 -0300

    feat(calculator): add square set methods

--- shape_calculator.py
+++ shape_calculator.py
@@ -32,3 +32,19 @@ class Rectangle:
         return f'Rectangle(width={self.width}, height={self.height})'
 
 class Square(Rectangle):
+    def __init__(self, side):
+        self.width = side
+        self.height = side
+    
+    def set_side(self, side):
+        self.width = side
+        self.height = side
+    
+    def set_width(self, width):
+        self.set_side(width)
+    
+    def set_height(self, height):
+        self.set_side(height)
+    
+    def __str__(self):
+        return f'Square(side={self.width})'
